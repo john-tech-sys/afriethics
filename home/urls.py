3d434d404066099c89e22e73fc9f536ca7b64557
GitHub User <github@example.com>
2026-03-31 19:23:18 +0300

    update with ping

--- home/urls.py
+++ home/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from .views import *
+from django.urls import path
+
 
 
 urlpatterns = [
-
 	path('', index, name='home'),
+    path("health/", health),
 	path('apply_for_cert', apply_for_cert, name='apply_for_cert'),
 	path('partnership', partnership, name='partnership'),
 	path('apply', apply, name='apply'),
